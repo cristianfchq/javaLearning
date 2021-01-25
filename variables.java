aeb1c3c34ee8175a98b960b5ea2f77c381049c22
cristianfchq <cristian.fchq@gmail.com>
2021-01-25 16:41:11 -0400

    file holaMundo.java and variable.java

---
+++ variables.java
@@ -0,0 +1,52 @@
+public class variables {
+
+    public static void main(String args []) {
+        
+        /**
+         * TIPOS DE DATOS PRIMITIVOS
+         */
+
+        byte entero = 12;
+        short entero1 = 231;
+        int entero2 = 321654;
+        long entero3 = 321654987;
+
+        System.out.println("Numero Entero : " + entero);
+        System.out.println("Numero Entero : " + entero1);
+        System.out.println("Numero Entero : " + entero2);
+        System.out.println("Numero Entero : " + entero3);
+
+        float decimal = 3.14f;
+        double decimal2 = 3.21550;
+
+        System.out.println("Numero Decimal : " + decimal);
+        System.out.println("Numero Decimal : " + decimal2);
+
+        boolean varBool = true;
+
+        System.out.println("La variable varBool es en : " + varBool);
+
+        char caracter = 'a';
+
+        System.out.println("Caracter : " + caracter);
+
+        /**
+         * TIPOS DE DATOS NO PRIMITIVOS
+         * estos tipos de dato tienen metodos
+         */
+
+        Integer valor = null;
+
+        System.out.println("La variable valor de tipo Integer tiene como valor : " + valor);
+
+        // valor.(aca nos muestra sus metodos)
+
+        String palabra = "Hola ketal como estas";
+
+        System.out.println("La variable palabra de tipo String tiene como valor : " + palabra);
+
+        // palabra.(aca nos muestra sus metodos del String)
+
+    }
+    
+}
